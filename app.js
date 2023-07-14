e2e0eeb224a511a7dddcfdd61789375f36d5e14c
dave-hickman <dave.robert.hickman@gmail.com>
2023-07-14 09:33:47 +0100

    add cors

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const cors = require('cors')
 const makeRequest = require('./controller/requestsController')
 
 const app = express()
+
+app.use(cors())
 
 app.use(express.json())
 
